1e381173604d67132f13b4cc53f5d53cced0826f
setos <shsetox@gmail.com>
2018-11-29 20:23:11 +0900

    created

---
+++ src/test/java/com/example/demo/AppTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue("".isEmpty());
+        assertTrue(true);
+    }
+
+}
